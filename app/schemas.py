0671555a0d0dbe1b5750a136603e5eaeed52f240
Pabl01802 <58702798+Pabl01802@users.noreply.github.com>
2025-06-29 18:41:25 +0200

    feat: modified schema

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from enum import Enum
+from typing import Annotated
+
+class ManufacturerEnum(str, Enum):
+  SIMAGIC = 'SIMAGIC'
+  MOZA = 'MOZA'
+
+class CategoryEnum(str, Enum):
+  WHEEL_BASE = 'Wheel base'
+  PEDALS = 'Pedals'
+  WHEELS = 'Wheels'
+
 
 class ProductCreate(BaseModel):
-  name: str
-  category: str
-  manufacturer: str
-  price: float
+  name: Annotated[str, Field(min_length=3, max_length=30)]
+  category: CategoryEnum
+  manufacturer: ManufacturerEnum
+  price: Annotated[float, Field(gt=0)]
